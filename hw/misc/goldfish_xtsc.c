54f7370690bd8aa0868a16456f56d9606a93e764
Max Filippov <jcmvbkbc@gmail.com>
2020-04-15 17:31:39 -0700

    goldfish_xtsc: support multiple devices with shared memory
    Add properties comm_addr and reserved_size to goldfish_xtsc device. When
    both properties are present CXRP0000 ACPI binding is used that
    corresponds to cdns,xrp OF binding. First (MMIO) register property is not
    used, and is the same as the second. Second register property specifies
    communication area and is one 4K page at 'comm_addr'. Third register
    property is shared memory location and is 'size - reserved_size' bytes
    at 'addr + reserved_size'. Shared memory size 'size' at 'addr' must
    match shared memory defined on the XTSC side. All references to the same
    piece of shared memory must have the same 'name', 'addr', 'size' and
    'reserved_size' properties.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- hw/misc/goldfish_xtsc.c
+++ hw/misc/goldfish_xtsc.c
@@ -19,6 +19,16 @@
 
 #define GOLDFISH_XTSC(obj) OBJECT_CHECK(goldfish_xtsc, (obj), TYPE_GOLDFISH_XTSC)
 
+typedef struct shared_memory {
+    int ref;
+    void *ram_ptr;
+    MemoryRegion memory;
+    hwaddr addr;
+    uint64_t size;
+    uint64_t reserved_size;
+    char *name;
+} SharedMemory;
+
 /**
   A goldfish pstore is currently an area of reservered memory that survives reboots. The memory
   is persisted to disk if the filename parameter has been given. We persist the memory region
@@ -26,60 +36,181 @@
 */
 typedef struct goldfish_xtsc {
     DeviceState parent;
-
-    void *ram_ptr;
-    MemoryRegion memory;
-    uint64_t size;   /* Size of the region in bytes, keep in mind the larger this
-                        gets, the longer writes on exit take */
-    hwaddr   addr;   /* Physical guest address where this memory shows up */
+    SharedMemory *shmem;
+    hwaddr addr;
+    uint64_t size;
     char *name;
+    hwaddr comm_addr;/* Communication area address */
+    uint64_t reserved_size;
 } goldfish_xtsc;
 
 static Property goldfish_xtsc_properties[] = {
     DEFINE_PROP_UINT64(GOLDFISH_XTSC_ADDR_PROP, goldfish_xtsc, addr, 0),
     DEFINE_PROP_SIZE(GOLDFISH_XTSC_SIZE_PROP, goldfish_xtsc, size, 0),
     DEFINE_PROP_STRING(GOLDFISH_XTSC_NAME_PROP, goldfish_xtsc, name),
+    DEFINE_PROP_UINT64(GOLDFISH_XTSC_COMM_ADDR_PROP, goldfish_xtsc, comm_addr, 0),
+    DEFINE_PROP_SIZE(GOLDFISH_XTSC_RESERVED_SIZE_PROP, goldfish_xtsc, reserved_size, 0),
     DEFINE_PROP_END_OF_LIST(),
 };
 
-static Object *obj;
+static SharedMemory mem[64];
+static Object *obj[64];
 
 Object *goldfish_xtsc_device(void)
+{
+    return obj[0];
+}
+
+Object **goldfish_xtsc_devices(void)
 {
     return obj;
 }
 
-hwaddr goldfish_xtsc_get_addr(Object *obj)
+hwaddr goldfish_xtsc_get_addr(Object *obj, int i)
 {
-  goldfish_xtsc *s = GOLDFISH_XTSC(obj);
-  return s->addr;
+    goldfish_xtsc *s = GOLDFISH_XTSC(obj);
+
+    switch (i) {
+    case 0:
+        return s->addr;
+
+    case 1:
+        return s->comm_addr;
+
+    default:
+        g_assert_not_reached();
+    }
 }
 
-uint64_t goldfish_xtsc_get_size(Object *obj)
+uint64_t goldfish_xtsc_get_size(Object *obj, int i)
 {
-  goldfish_xtsc *s = GOLDFISH_XTSC(obj);
-  return s->size;
+    goldfish_xtsc *s = GOLDFISH_XTSC(obj);
+
+    switch (i) {
+    case 0:
+        return s->size;
+
+    case 1:
+        return s->reserved_size;
+
+    default:
+        g_assert_not_reached();
+    }
 }
 
-static void goldfish_xtsc_realize(DeviceState *dev, Error **errp) {
-  goldfish_xtsc *s = GOLDFISH_XTSC(dev);
+static SharedMemory *find_shared_memory(const char *name,
+                                        hwaddr addr,
+                                        uint64_t size,
+                                        uint64_t reserved_size)
+{
+    size_t i;
 
-  s->ram_ptr = xtsc_open_shared_memory(s->name ? s->name : "SharedRAM_L",
-				       s->size);
-  // Reserve a slot of memory that we will use for XTSC shared memory.
-  memory_region_init_ram_ptr(&s->memory, OBJECT(s), GOLDFISH_XTSC_DEV_ID, s->size,
-			     s->ram_ptr);
-
-  // Ok, now we just need to move it to the right physical address.
-  memory_region_add_subregion(get_system_memory(), s->addr, &s->memory);
-  obj = OBJECT(dev);
+    if (addr + size < addr) {
+        fprintf(stderr,
+                "%s: %s, %" HWADDR_PRIx "/%" PRIx64 ": wrap around\n",
+                __func__, name, addr, size);
+        abort();
+    }
+    if (size < reserved_size) {
+        fprintf(stderr,
+                "%s: %s, %" HWADDR_PRIx "/%" PRIx64 ": reserved size exceeds memory size\n",
+                __func__, name, addr, size);
+        abort();
+    }
+    for (i = 0; i < ARRAY_SIZE(mem); ++i)
+        if (mem[i].ref) {
+            if (strcmp(mem[i].name, name) == 0 &&
+                mem[i].addr == addr &&
+                mem[i].size == size &&
+                mem[i].reserved_size == reserved_size)
+                return mem + i;
+            if (addr < mem[i].addr + mem[i].size &&
+                addr + size > mem[i].addr) {
+                fprintf(stderr,
+                        "%s: %s, %" HWADDR_PRIx "/%" PRIx64 ": overlaps other area without exact match\n",
+                        __func__, name, addr, size);
+                abort();
+            }
+        }
+    return NULL;
 }
 
-static void goldfish_xtsc_unrealize(DeviceState *dev, Error **errp) {
-  goldfish_xtsc *s = GOLDFISH_XTSC(dev);
+static SharedMemory *find_or_open_shared_memory(const char *name,
+                                                hwaddr addr,
+                                                uint64_t size,
+                                                uint64_t reserved_size)
+{
+    size_t i;
+    SharedMemory *pmem;
 
-  memory_region_del_subregion(get_system_memory(), &s->memory);
-  obj = NULL;
+    if (!name)
+        name = "SharedRAM_L";
+    pmem = find_shared_memory(name, addr, size, reserved_size);
+
+    if (pmem) {
+        ++pmem->ref;
+        return pmem;
+    }
+
+    for (i = 0; i < ARRAY_SIZE(mem); ++i)
+        if (mem[i].ref == 0)
+            break;
+
+    pmem = mem + i;
+
+    pmem->ref = 1;
+    pmem->name = strdup(name);
+    pmem->addr = addr;
+    pmem->size = size;
+    pmem->reserved_size = reserved_size;
+
+    pmem->ram_ptr = xtsc_open_shared_memory(pmem->name, size);
+    // Reserve a slot of memory that we will use for XTSC shared memory.
+    memory_region_init_ram_ptr(&mem->memory, NULL, GOLDFISH_XTSC_DEV_ID, size,
+                               pmem->ram_ptr);
+
+    // Ok, now we just need to move it to the right physical address.
+    memory_region_add_subregion(get_system_memory(), addr, &mem->memory);
+
+    return pmem;
+}
+
+static void release_shared_memory(SharedMemory *pmem)
+{
+    if (pmem->ref == 1) {
+        memory_region_del_subregion(get_system_memory(), &pmem->memory);
+        free(pmem->name);
+    }
+    --pmem->ref;
+}
+
+static void goldfish_xtsc_realize(DeviceState *dev, Error **errp)
+{
+    goldfish_xtsc *s = GOLDFISH_XTSC(dev);
+    unsigned i;
+
+    s->shmem = find_or_open_shared_memory(s->name, s->addr,
+                                          s->size, s->reserved_size);
+
+    for (i = 0; i < ARRAY_SIZE(obj); ++i)
+        if (obj[i] == NULL) {
+            obj[i] = OBJECT(dev);
+            break;
+        }
+}
+
+static void goldfish_xtsc_unrealize(DeviceState *dev, Error **errp)
+{
+    goldfish_xtsc *s = GOLDFISH_XTSC(dev);
+    unsigned i, j;
+
+    release_shared_memory(s->shmem);
+
+    for (i = j = 0; i < ARRAY_SIZE(obj); ++i)
+        if (obj[i] != OBJECT(dev))
+            obj[j++] = obj[i];
+    while (j < ARRAY_SIZE(obj))
+        obj[j++] = NULL;
 }
 
 static void goldfish_dev_init(Object *obj) {
